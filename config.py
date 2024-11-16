d74732d07e30dc74ad054e6caaee1d0c4a477b41
Saurav Kumar <arthur14202000@gmail.com>
2024-11-16 19:48:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,6 +8,9 @@ FAISS_INDEX_PATH = "faiss_index.bin"
 
 # API configuration
 from dotenv import load_dotenv
+import os
+
+# get your API Key and API Url from https://sambanova.ai/ and write the key and url in `.env` file
 load_dotenv()
 SAMBANOVA_API_URL = os.getenv('SAMBANOVA_API_URL')
 SAMBANOVA_API_KEY = os.getenv('SAMBANOVA_API_KEY')
